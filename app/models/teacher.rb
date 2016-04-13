a5eab7ac099bb833a8a15d5898a118cf774a5466
Matt Zollinhofer <matt.zollinhofer@gmail.com>
2016-04-12 21:42:02 -0400

    Add student relationship to teacher

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,4 +2,5 @@ class Teacher < ActiveRecord::Base
   belongs_to :user
   has_many :teacher_courses
   has_many :courses, through: :teacher_courses
+  has_many :students, through: :courses
 end
--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -4,4 +4,5 @@ RSpec.describe Teacher do
   it { is_expected.to belong_to :user }
   it { is_expected.to have_many :teacher_courses }
   it { is_expected.to have_many(:courses).through :teacher_courses }
+  it { is_expected.to have_many(:students).through :courses }
 end
