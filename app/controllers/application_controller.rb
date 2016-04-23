efcac57b1030f1d256eb83139c870e8e94ff06c3
Matt Zollinhofer <matt.zollinhofer@gmail.com>
2016-04-23 18:03:22 -0400

    Customize devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,13 @@ class ApplicationController < ActionController::Base
 
   # Devise authenticaiton on all controller actions
   before_action :authenticate_user!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :mobile_number])
+  end
+
 end
