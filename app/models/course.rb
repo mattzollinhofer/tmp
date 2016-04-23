73ecceb13562ac2f5c40c59ce0d586608b4dc0a1
Matt Zollinhofer <matt.zollinhofer@gmail.com>
2016-04-23 11:43:42 -0400

    Add pretty printing to course

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,4 +4,8 @@ class Course < ActiveRecord::Base
   has_many :students, through: :student_courses
   has_many :teacher_courses
   has_many :teachers, through: :teacher_courses
+
+  def to_s
+    "#{name}, taught by #{teachers.map(&:user).map(&:first_name).join(', ')}"
+  end
 end
