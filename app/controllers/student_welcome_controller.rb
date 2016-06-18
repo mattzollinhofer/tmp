3b6a26bdb541f3e2f33141052f4b51b14b4744f4
Matt Zollinhofer <matt.zollinhofer@gmail.com>
2016-06-17 23:41:54 -0400

    Removing dead code after refactor

--- app/controllers/student_welcome_controller.rb
+++
@@ -1,5 +0,0 @@
-class StudentWelcomeController < ApplicationController
-  def index
-    @student_welcome = StudentWelcome.new Student.find_by(user: current_user)
-  end
-end
--- app/controllers/teacher_welcome_controller.rb
+++
@@ -1,5 +0,0 @@
-class TeacherWelcomeController < ApplicationController
-  def index
-    @teacher_welcome = TeacherWelcome.new Teacher.find_by(user: current_user)
-  end
-end
