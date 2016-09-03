eb1c019a4249ed5d7bc5594a8106745c8d03e138
Matt Zollinhofer <matt.zollinhofer@gmail.com>
2016-09-03 11:29:56 -0400

    Whitespace cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :lockable, :omniauthable, :omniauth_providers => [:google_oauth2]
+    :recoverable, :rememberable, :trackable, :validatable,
+    :lockable, :omniauthable, :omniauth_providers => [:google_oauth2]
 
   validates :first_name, presence: true
   validates :last_name, presence: true
