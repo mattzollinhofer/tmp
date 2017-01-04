76ad91f59b8087554c34194ee8c02823822b3f22
Matt Zollinhofer <matt.zollinhofer@gmail.com>
2017-01-03 22:47:50 -0500

    Automatically update the percentage when updating the gradebook
    Resolves #43.

--- app/controllers/class_assignment_controller.rb
+++ app/controllers/class_assignment_controller.rb
@@ -2,8 +2,8 @@ class ClassAssignmentController < ApplicationController
   def update
     class_assignment = ClassAssignment.find params[:id]
     if class_assignment.update_attributes(class_assignment_params)
+      @gradebook = Gradebook.new(class_assignment.student_class.class_period, class_assignment.assignment.unit)
       @student = class_assignment.student_class.student
-      @total_points = ClassAssignment.all_points_for_unit(class_assignment.student_class, class_assignment.assignment.unit)
       flash[:success] = 'Updated assignment'
     else
       flash[:error] = 'Failed to update assignment'
