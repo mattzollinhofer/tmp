44738bc443eb8758ab76199eacfebc708cda5419
Matt Zollinhofer <matt.zollinhofer@gmail.com>
2017-02-03 19:45:10 -0500

    Cleanup deprecation warning

--- config/application.rb
+++ config/application.rb
@@ -29,9 +29,6 @@ module Teachy
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-
     config.autoload_paths += Dir[Rails.root.join('app', 'models', '{*/}')]
 
     # Try loading a YAML file at `./config/env.[environment].yml`, if it exists
