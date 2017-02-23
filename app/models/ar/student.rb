4da4042f6983c16d14bfc7f5cdf603a0bf5701ca
Matt Zollinhofer <matt.zollinhofer@gmail.com>
2017-02-22 22:39:05 -0500

    Remove what support there was for graduation year

--- app/models/ar/student.rb
+++ app/models/ar/student.rb
@@ -2,16 +2,6 @@ class Student < User
   has_many :student_classes
   has_many :class_periods, through: :student_classes
 
-#  validates :graduation_year, presence: true,
-#                              inclusion: {
-#                                in: Date.today.year..(Date.today + 30.years).year,
-#                                message: 'must be between this year and 30 years from now'
-#                              },
-#                              numericality: {only_integer: true},
-#                              format:{
-#                                with: /20\d{2}/i,
-#                                message: 'must be a four digit year'
-#                              }
   def display_name
     nickname || "#{last_name}, #{first_name}"
   end
