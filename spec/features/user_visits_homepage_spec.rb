2d816a8ba488aa5a95f8f8f3dfc2a57cbc30d712
Matt Zollinhofer <matt.zollinhofer@gmail.com>
2019-11-01 23:24:42 -0400

    Update bundler and ruby versions

--- spec/features/user_visits_homepage_spec.rb
+++ spec/features/user_visits_homepage_spec.rb
@@ -4,7 +4,7 @@ feature 'User Visits Homepage' do
   scenario 'successfully' do
     visit '/'
 
-    expect(page).to have_css '.h1', text: 'Sign In!!'
+    expect(page).to have_css '.h1', text: 'Sign In!'
     expect(page).to have_css 'a', text: 'With Gmail'
   end
 end
